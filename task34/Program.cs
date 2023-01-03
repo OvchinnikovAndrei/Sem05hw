c0e8576a30fb7e2cf0aaaf3c3feb5905ceea4ede
Andrey Ovchinnikov <sunnyhand@bk.ru>
2023-01-03 18:59:23 +0300

    ДЗ к семинару 5

---
+++ task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+Console.Write("Количество элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] arr = new int[size];
+RandomNumbersArray(arr);
+PrintArray(arr);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+if (arr[i] % 2 == 0)
+count++;
+
+Console.Write($"{count} чётных числа");
+
+void RandomNumbersArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int k = 0; k < arr.Length; k++)
+    {
+        // Console.Write($"{arr[i]} ");
+        if (k != arr.Length-1)
+        {
+            Console.Write($"{arr[k]} ");
+        }
+        else
+        {
+            Console.Write($"{arr[k]}");
+        }    
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ task36/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.Write("Количество элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] arr = new int[size];
+RandomNumbersArray(arr);
+PrintArray(arr);
+int sum = 0;
+
+for (int i = 0; i < arr.Length; i+=2)
+    sum = sum + arr[i];
+
+    Console.Write($"Сумма элементов на нечётных позициях: {sum}");
+
+void RandomNumbersArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int k = 0; k < arr.Length; k++)
+      if (k != arr.Length-1)
+        {
+            Console.Write($"{arr[k]} ");
+        }
+        else
+        {
+            Console.Write($"{arr[k]}");
+        }    
+    Console.Write("]");
+    Console.WriteLine();
+}
